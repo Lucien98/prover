f542e5847430e31a34b74f79e51edcc99b0b236c
lucien98 <2172856294@qq.com>
2023-07-25 02:51:05 -0700

    rename folder

--- experiment/varorder/run_improved.sh
+++ experiment/varorder/run_improved.sh
@@ -4,5 +4,5 @@
 while read line;
 do
 	echo -n $line, >> experiment/count_node/improved.csv
-	./bin/verify --insfile ${line} --count_node 1 --improve_varorder 1 --verbose 0 --memory 8589934592 >> experiment/count_node/improved.csv
+	./bin/verify --insfile ${line} --count_node 0 --improve_varorder 1 --verbose 0 --memory 8589934592 >> experiment/count_node/improved.csv
 done < experiment/count_node/benchs.txt
--- experiment/varorder/run_original.sh
+++ experiment/varorder/run_original.sh
@@ -4,6 +4,6 @@
 while read line;
 do
 	echo -n $line, >> experiment/count_node/original.csv
-	./bin/verify --insfile ${line} --count_node 1 --improve_varorder 0 --verbose 0 --memory 8589934592 >> experiment/count_node/original.csv
+	./bin/verify --insfile ${line} --count_node 0 --improve_varorder 0 --verbose 0 --memory 8589934592 >> experiment/count_node/original.csv
 
 done < experiment/count_node/benchs.txt
