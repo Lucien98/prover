64c7aaf3fb661d8b319b36585b133d8b7a970d3d
lucien98 <2172856294@qq.com>
2024-03-24 01:30:11 -0700

    add results

---
+++ experiment/TCHES/verify/run.sh
@@ -0,0 +1,2 @@
+bash experiment/TCHES/verify/run_prover.sh 
+bash experiment/TCHES/verify/run_silver.sh 
---
+++ experiment/TCHES/verify/run_silver_all.sh
@@ -0,0 +1,2 @@
+bash experiment/TCHES/verify/run_silver_smallsboxes.sh 
+bash experiment/TCHES/verify/run_silver_largersboxes.sh 
