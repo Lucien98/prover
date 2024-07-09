b204aae606c1ef4bc0518ef3bf4e2f8451bab507
lucien98 <2172856294@qq.com>
2024-07-09 00:47:28 -0700

    add results excpet the results of silver verifying two-sboxes version in LowRandomMasking

--- experiment/NullFresh2/run_silver.sh
+++ experiment/NullFresh2/run_silver.sh
@@ -2,6 +2,6 @@
 while read line;
 do
 	echo -n $line, >> experiment/NullFresh2/silver.csv
-	./bin/verify --insfile ${line} --varorder 0 --userule 0 --timeout 20 --verbose 0 --memory 8589934592 >> experiment/NullFresh2/silver.csv
+	./bin/verify --insfile ${line} --varorder 0 --userule 0 --timeout 1440 --verbose 0 --memory 8589934592 >> experiment/NullFresh2/silver.csv
 
 done < experiment/NullFresh2/benchs.txt
