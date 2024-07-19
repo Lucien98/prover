5b772ebd99f097ec0596ba4d8eb277f0ce160de9
lucien98 <2172856294@qq.com>
2024-07-19 00:00:42 -0700

    the data for table 4 of SILVER

--- experiment/TCHES/verify/run_prover.sh
+++
@@ -1,8 +0,0 @@
-# run `bash experiment/run.sh` in root folder of the project
-
-while read line;
-do
-	echo -n $line, >> experiment/TCHES/verify/prover.csv
-	./bin/verify --insfile ${line} --timeout 20 --verbose 0 --memory 8589934592 >> experiment/TCHES/verify/prover.csv
-
-done < experiment/TCHES/verify/benchmarks_sboxes.txt
