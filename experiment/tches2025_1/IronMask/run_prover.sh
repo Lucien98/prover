be82f848aa080959cac9f5315b8d3b24befc0528
lucien98 <2172856294@qq.com>
2024-08-22 20:08:45 -0700

    add results for comparison to IronMask

--- experiment/tches2025_1/IronMask/run_prover.sh
+++ experiment/tches2025_1/IronMask/run_prover.sh
@@ -7,11 +7,11 @@ do
 done < experiment/tches2025_1/IronMask/benchs.txt
 
 echo -n "test/ISW_mult/6shares.nl," >> experiment/tches2025_1/IronMask/results.csv
+./bin/verify --verbose 0 --insfile test/ISW_mult/6shares.nl --notion 0 >> experiment/tches2025_1/IronMask/results.csv
 ./bin/verify --verbose 0 --insfile test/ISW_mult/6shares.nl --notion 1 >> experiment/tches2025_1/IronMask/results.csv
 ./bin/verify --verbose 0 --insfile test/ISW_mult/6shares.nl --notion 2 >> experiment/tches2025_1/IronMask/results.csv
-echo "" >> experiment/tches2025_1/IronMask/results.csv
 
 echo -n "test/refresh_nlogn/7shares.nl," >> experiment/tches2025_1/IronMask/results.csv
+./bin/verify --verbose 0 --insfile test/refresh_nlogn/7shares.nl --notion 0 >> experiment/tches2025_1/IronMask/results.csv
 ./bin/verify --verbose 0 --insfile test/refresh_nlogn/7shares.nl --notion 1 >> experiment/tches2025_1/IronMask/results.csv
 ./bin/verify --verbose 0 --insfile test/refresh_nlogn/7shares.nl --notion 2 >> experiment/tches2025_1/IronMask/results.csv
-echo "" >> experiment/tches2025_1/IronMask/results.csv
